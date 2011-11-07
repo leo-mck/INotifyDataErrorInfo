b18d90cf21b10c3da0f4706ce5987648ae7f4447
Leo <ateista@gmail.com>
2011-11-07 17:19:05 -0200

    'first'

---
+++ ErrorConverter.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="ErrorConverter.cs" company="$registerdorganization$">
+// Copyright (c) 2011 . All Right Reserved
+// </copyright>
+// <author>Leo</author>
+// <email></email>
+// <date>2011-07-23</date>
+// <summary>A value converter for WPF and Silverlight data binding</summary>
+
+namespace GridTest
+{
+    using System;
+    using System.Windows.Data;
+using System.Windows.Media;
+
+    /// <summary>
+    /// A Value converter
+    /// </summary>
+    public class ErrorConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        /// <summary>
+        /// Modifies the source data before passing it to the target for display in the UI. 
+        /// </summary>
+        /// <param name="value">The source data being passed to the target </param>
+        /// <param name="targetType">The Type of data expected by the target dependency property.</param>
+        /// <param name="parameter">An optional parameter to be used in the converter logic.</param>
+        /// <param name="culture">The culture of the conversion.</param>
+        /// <returns>The value to be passed to the target dependency property. </returns>
+        /// 
+        SolidColorBrush error = new SolidColorBrush(Colors.Red);
+        SolidColorBrush normal = new SolidColorBrush(Colors.Black);
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var item = value as Item;
+
+            if (item.HasErrors)
+                return error;
+            else
+                return normal;
+        }
+
+        /// <summary>
+        /// Modifies the target data before passing it to the source object. This method is called only in TwoWay bindings. 
+        /// </summary>
+        /// <param name="value">The target data being passed to the source.</param>
+        /// <param name="targetType">The Type of data expected by the source object.</param>
+        /// <param name="parameter">An optional parameter to be used in the converter logic. </param>
+        /// <param name="culture">The culture of the conversion.</param>
+        /// <returns>The value to be passed to the source object.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Threading;
+using System.Windows.Threading;
+using System.ComponentModel.DataAnnotations;
+
+namespace GridTest
+{
+    public partial class MainPage : UserControl
+    {
+
+
+        public ObservableCollection<Item> Data { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+
+
+
+            Data = new ObservableCollection<Item>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var it = new Item() { Value = i, Name = "", IsReady = false };
+
+                Data.Add(it);
+            }
+
+
+            this.DataContext = this;
+        }
+
+
+
+
+    }
+
+
+
+    public class Item : INotifyPropertyChanged, INotifyDataErrorInfo
+    {
+
+        Dictionary<string, string> _errors = new Dictionary<string, string>();
+        
+        bool isNotifing = false;
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value != name && isNotifing)
+                {
+                    name = value;
+                    var propertyName = "Name";
+                    ValidateAsync(propertyName, value);
+                }
+                isNotifing = true;
+            }
+        }
+
+
+
+        
+
+        private void ValidateAsync(string propertyName, string value)
+        {
+
+            if (propertyName == "Name")
+            {
+                BackgroundWorker bw = new BackgroundWorker();
+                bw.DoWork += (sd, args) =>
+                {
+                    //simulate an async request
+                    Thread.Sleep(1000);
+                    args.Result = args.Argument;
+                };
+
+                bw.RunWorkerCompleted += (sd, args) =>
+                {
+                    var val = args.Result as string;
+
+                    if (val != "VALID" && val != "")
+                    {                        
+                        _errors.Add("Name",  "Invalid Name" );                        
+                    }
+                    else
+                    {
+                        _errors.Remove(propertyName);
+                    }
+                    
+                    NotifyErrorsChanged("Name");
+                    NotifyPropertyChanged("HasErrors");                    
+
+                };
+
+                bw.RunWorkerAsync(value);
+
+            }
+        }
+
+
+        public int Value { get; set; }
+        public bool IsReady { get; set; }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            
+            if (!String.IsNullOrWhiteSpace(propertyName) && _errors.ContainsKey(propertyName))
+            {                
+                return _errors.Values;
+            }
+            else
+                return null;
+        }
+
+        public bool HasErrors
+        {
+            get { return _errors.Count > 0; }
+        }
+
+
+
+        private void NotifyErrorsChanged(string propertyName)
+        {
+
+            if (ErrorsChanged != null)
+            {
+                ErrorsChanged(this, new DataErrorsChangedEventArgs(propertyName));
+            }
+
+        }
+
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
